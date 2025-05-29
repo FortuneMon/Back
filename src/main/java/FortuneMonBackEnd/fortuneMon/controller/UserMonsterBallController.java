698e23ea7e1d69f7f91f0c56c9acb5285f6c33ac
Seung-Un <un7476@naver.com>
2025-05-30 00:25:09 +0900

    chore: #23 swagger 설명 작성
    swagger 설명 작성성

--- src/main/java/FortuneMonBackEnd/fortuneMon/controller/UserMonsterBallController.java
+++ src/main/java/FortuneMonBackEnd/fortuneMon/controller/UserMonsterBallController.java
@@ -4,6 +4,7 @@ import FortuneMonBackEnd.fortuneMon.DTO.UserMonsterBallResponseDTO;
 import FortuneMonBackEnd.fortuneMon.DTO.UserPokemonDTO;
 import FortuneMonBackEnd.fortuneMon.config.security.SecurityUtil;
 import FortuneMonBackEnd.fortuneMon.service.UserMonsterBallService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,8 @@ import java.util.List;
 public class UserMonsterBallController {
     private final UserMonsterBallService userMonsterBallService;
 
+    @Operation(summary = "유저가 가진 몬스터볼 개수 조회", description =
+            "# 유저 몬스터볼 조회 API입니다. 로그인 후 진행하세요.")
     @GetMapping("")
     public List<UserMonsterBallResponseDTO> getUserMonsterBall(){
         Long userId = SecurityUtil.getCurrentUserId();
@@ -23,6 +26,8 @@ public class UserMonsterBallController {
         return userMonsterBallService.getUserMonsterBall(userId);
     }
 
+    @Operation(summary = "몬스터볼 오픈", description =
+            "# 몬스터볼을 오픈하는 API입니다. 로그인 후 진행하세요.")
     @PostMapping("/{id}/open")
     public UserPokemonDTO openMonsterBall(@PathVariable("id") Long ballId){
         return userMonsterBallService.openMonsterBall(ballId);
--- src/main/java/FortuneMonBackEnd/fortuneMon/controller/UserPokemonController.java
+++ src/main/java/FortuneMonBackEnd/fortuneMon/controller/UserPokemonController.java
@@ -3,6 +3,7 @@ package FortuneMonBackEnd.fortuneMon.controller;
 import FortuneMonBackEnd.fortuneMon.DTO.UserPokemonDTO;
 import FortuneMonBackEnd.fortuneMon.config.security.SecurityUtil;
 import FortuneMonBackEnd.fortuneMon.service.UserPokemonServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,12 +17,16 @@ public class UserPokemonController {
 
     // 요청 받으면 Service 호출해서 유저가 보유한 포켓몬을 표시하여 반환함
     // 유저의 id를 받아서 id를 기준으로 유저의 포켓몬 정보를 가져온다
+    @Operation(summary = "유저 포켓몬 조회", description =
+            "# 유저 포켓몬 조회 API입니다. 로그인 후 진행하세요.")
     @GetMapping("/pokemons")
     public List<UserPokemonDTO> getPokemonsWithOwnership(){
         Long userId = SecurityUtil.getCurrentUserId();
         return userPokemonService.getUserPokemonsWithOwnership(userId);
     }
 
+    @Operation(summary = "파트너 포켓몬 설정", description =
+            "# 파트너 포켓몬 설정 API입니다. 로그인 후 진행하세요.")
     @PatchMapping("/partners/{id}")
     public UserPokemonDTO setPartnerPokemon(@PathVariable("id") Long pokemonId){
         return userPokemonService.setPartnerPokemon(pokemonId);
