43afcecb1fa054f95f080eca48d19cf632caa00f
mino <dlalsgh6772@gmail.com>
2025-06-11 14:27:00 +0900

    프론트 앤드 배포 url 등록 (webconfig, securityconfig)

--- src/main/java/FortuneMonBackEnd/fortuneMon/config/WebConfig.java
+++ src/main/java/FortuneMonBackEnd/fortuneMon/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://localhost:3000") // 프론트엔드 주소 추가
+                        .allowedOrigins("http://localhost:5173", "http://localhost:3000", "https://fortunemon.vercel.app") // 프론트엔드 주소 추가
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
